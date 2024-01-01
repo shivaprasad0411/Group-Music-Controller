c99058c5fada8ef3db717216cb770bf98f2f153a
Shivaprasad Naikawadi <shivaprasad@Shivaprasads-MacBook-Pro.local>
2024-01-01 18:38:46 +0530

    Calling API Endpoints From React

---
+++ frontend/src/components/Room.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+        };
+        this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails = this.getRoomDetails.bind(this);
+        this.getRoomDetails();
+    }
+
+    getRoomDetails() {
+        fetch('/api/get-room' + '?code=' + this.roomCode)
+            .then((response) => {
+                if (!response.ok) {
+                    this.props.leaveRoomCallback();
+                    this.props.history.push('/');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pause,
+                    isHost: data.is_host,
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.roomCode}</h3>
+                <p>Votes: {this.state.votesToSkip}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
+            </div>
+        );
+    }
+}
