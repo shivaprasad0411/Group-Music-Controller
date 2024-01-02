45c39d2e0f34f4a1f03e005f682159838d3e193b
Naikawadi Shivaprasad <prasad.4@iitj.ac.in>
2024-01-02 11:54:26 +0530

    Add Update Room functionality

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import {Grid, Button, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 
 export default class Room extends Component {
     constructor(props) {
@@ -9,11 +7,28 @@ export default class Room extends Component {
             votesToSkip: 2,
             guestCanPause: false,
             isHost: false,
+            showSettings: false
         };
         this.roomCode = this.props.match.params.roomCode;
         this.getRoomDetails = this.getRoomDetails.bind(this);
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+        this.updateShowSettings = this.updateShowSettings.bind(this);
+        this.renderSettingsButton = this.renderSettingsButton.bind(this);
+        this.renderSettings = this.renderSettings.bind(this);
         this.getRoomDetails();
         this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    }
+
+    leaveButtonPressed() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/api/leave-room', requestOptions)
+            .then((_response) => {
+                this.props.leaveRoomCallback();
+                this.props.history.push('/');
+            });
     }
 
     leaveButtonPressed() {
@@ -46,41 +61,54 @@ export default class Room extends Component {
             });
     }
 
-    render() {
+    updateShowSettings(value) {
+        this.setState({
+            showSettings: value,
+        });
+    }
+
+    renderSettingsButton() {
+        return (
+            <Grid item xs={12} align='center'>
+                <Button variant='contained' color='primary' onClick={() => this.updateShowSettings(true)}>
+                    Settings
+                </Button>
+            </Grid>
+        );
+    }
+
+    renderSettings() {
         return (
             <Grid container spacing={1}>
                 <Grid item xs={12} align='center'>
-                    <Typography variant='h4' compact='h4'>
-                        Code: {this.roomCode}
-                    </Typography>
+                    <CreateRoomPage 
+                        update={true} 
+                        votesToSkip={this.state.votesToSkip} 
+                        guestCanPause={this.state.guestCanPause} 
+                        roomCode={this.roomCode}
+                        updateCallback={this.getRoomDetails}
+                    />
                 </Grid>
                 <Grid item xs={12} align='center'>
-                    <Typography variant='h6' compact='h6'>
-                        Votes: {this.state.votesToSkip}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Typography variant='h6' compact='h6'>
-                        Guest Can Pause: {this.state.guestCanPause.toString()}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Typography variant='h6' compact='h6'>
-                        Host: {this.state.isHost.toString()}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Button color='secondary' variant='contained' onClick={this.leaveButtonPressed}>
-                        Leave Room
+                    <Button variant='contained' color='secondary' onClick={() => this.updateShowSettings(false)}>
+                        Close
                     </Button>
                 </Grid>
             </Grid>
-            // <div>
-            //     <h3>{this.roomCode}</h3>
-            //     <p>Votes: {this.state.votesToSkip}</p>
-            //     <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-            //     <p>Host: {this.state.isHost.toString()}</p>
-            // </div>
+        );
+    }
+
+    render() {
+        if (this.state.showSettings) {
+            return this.renderSettings();
+        }
+        return (
+            <div>
+                <h3>{this.roomCode}</h3>
+                <p>Votes: {this.state.votesToSkip}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
+            </div>
         );
     }
 }
