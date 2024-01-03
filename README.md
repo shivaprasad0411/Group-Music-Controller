ef4b86432a0d91fb572ec13dee60c5a773addc6e
Naikawadi Shivaprasad <prasad.4@iitj.ac.in>
2024-01-03 13:38:01 +0530

    Add Info Page and Update README

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Group-Music-Controller
+
+This repository contains the code for developing a application for group music sessions where users can join a room and listen to the music in a group and can also change the song if a particular amount of users agree.
+
+## Setup Instructions
+
+### Install Required Python Modules
+
+```bash
+pip install -r requirements.txt
+```
+### Start Web Server
+
+To start the web server you need to run the following sequence of commands.
+
+- In the Group-Music-Controller directory run the following command.
+```bash
+python3 manage.py runserver
+```
+
+### [Install Node.js](https://nodejs.org/en/)
+
+### Install Node Modules
+
+First cd into the ```frontend``` folder.
+```bash
+cd frontend
+```
+Next install all dependicies.
+```bash
+npm i
+```
+
+### Compile the Front-End
+
+Run the production compile script
+```bash
+npm run build
+```
+or for development:
+```bash
+npm run dev
+```
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -10,6 +10,7 @@ import {
     Redirect
 } from 'react-router-dom';
 import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
+import Info from './info';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -43,6 +44,9 @@ export default class HomePage extends Component {
                         <Button color='primary' to='/join' component={Link}>
                             Join a Room
                         </Button>
+                        <Button color='default' to='/info' component={Link}>
+                            Info
+                        </Button>
                         <Button color='secondary' to='/create' component={Link}>
                             Create a Room
                         </Button>
@@ -75,6 +79,7 @@ export default class HomePage extends Component {
                     }
                 }/>
                 <Route path='/join' component={RoomJoinPage}/>
+                <Route path='/info' component={Info}/>
                 <Route path='/create' component={CreateRoomPage}/>
                 <Route path='/room/:roomCode'
                 render = {
